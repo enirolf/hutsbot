0d1cc43d3ec539acfb9ce0e515b8018ffcff55f5
Florine de Geus <florinedegeus@gmail.com>
2020-10-03 21:35:03 +0200

    Remove strict host checking to prevent TravisCI from hanging

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-ssh -i ./deploy_key florine@$SERVER_IP
+ssh -i ./deploy_key florine@$SERVER_IP -o StrictHostKeyChecking=no
 docker pull $DOCKER_IMAGE:latest
 docker run -d $DOCKER_IMAGE:latest ./run_bot.sh
