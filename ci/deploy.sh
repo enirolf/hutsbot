c99e237de9c701ee6cb10c1200426c13f69717dd
Florine de Geus <florinedegeus@gmail.com>
2020-10-04 11:57:48 +0200

    Hopefully fix deploy this time for real

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 eval "$(ssh-agent -s)"
 chmod 600 /tmp/deploy_key
+echo -e "Host $SERVER_IP\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 ssh-add /tmp/deploy_key
 
-ssh -i /tmp/deploy_key git@$SERVER_IP pwd -o StrictHostKeyChecking=no
+ssh -i /tmp/deploy_key git@$SERVER_IP pwd
 docker pull $DOCKER_IMAGE:latest
 docker run -d $DOCKER_IMAGE:latest ./run_bot.sh
