ed1ea7d36d0d5aae530feaaae93dc66a3c2c0f2f
Florine de Geus <florinedegeus@gmail.com>
2020-10-05 17:00:24 +0200

    Lets try this

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -3,6 +3,9 @@ eval "$(ssh-agent -s)"
 chmod 600 /tmp/deploy_key
 ssh-add /tmp/deploy_key
 
-ssh -i /tmp/deploy_key git@$SERVER_IP pwd -o StrictHostKeyChecking=no
-docker pull $DOCKER_IMAGE:latest
-docker run -d $DOCKER_IMAGE:latest ./run_bot.sh
+ssh -i /tmp/deploy_key git@$SERVER_IP -o StrictHostKeyChecking=no <<EOF
+  mkdir -p hutsbot
+  cd hutsbot
+  docker pull $DOCKER_IMAGE:latest
+  docker run -d $DOCKER_IMAGE:latest ./run_bot.sh
+EOF
